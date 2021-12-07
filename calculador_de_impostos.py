23c11b20e1fd94646a3aaf59a80f1067dee6a1fb
ibsenriou <ibsenriou@hotmail.com>
2021-12-07 16:05:59 -0300

    Fim da Aula 3

--- calculador_de_impostos.py
+++ calculador_de_impostos.py
@@ -9,12 +9,22 @@ class CalculadorDeImpostos(object):
 
 if __name__ == '__main__':
 
-    from orcamento import Orcamento
-    from impostos import ISS, ICMS
+    from orcamento import Orcamento, Item
+    from impostos import ISS, ICMS, ICPP, IKCV
 
-    orcamento = Orcamento(500)
+    orcamento = Orcamento()
+
+    orcamento.adiciona_item(Item('ITEM 1', 50))
+    orcamento.adiciona_item(Item('ITEM 2', 200))
+    orcamento.adiciona_item(Item('ITEM 3', 250))
 
     calculador_de_impostos = CalculadorDeImpostos()
+    print('ISS e ICMS')
 
     calculador_de_impostos.realiza_calculo(orcamento, ISS())
     calculador_de_impostos.realiza_calculo(orcamento, ICMS())
+
+    print('ICPP e ICKV')
+
+    calculador_de_impostos.realiza_calculo(orcamento, ICPP())
+    calculador_de_impostos.realiza_calculo(orcamento, IKCV())
